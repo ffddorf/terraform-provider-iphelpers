ab96cc87a5d65c3018133b22ec2c1904b251d736
Marcus Weiner <marcus.weiner@gmail.com>
2024-02-03 15:48:13 +0100

    Document provider

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
+// Run the docs generation tool, check its repository for more information on how it works and how docs
+// can be customized.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate -provider-name iphelpers
+
 var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary.
---
+++ templates/index.md
@@ -0,0 +1,9 @@
+---
+page_title: "Provider: IP Helpers"
+description: |-
+  Helper functions for dealing with IP addressing in Terraform.
+---
+
+# IP Helpers Provider
+
+Helper functions for dealing with IP addressing in Terraform.
